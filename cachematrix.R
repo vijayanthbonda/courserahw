cb11690011058638f4fce902ebbc27080c2ab1ab
vijayanthbonda <64548579+vijayanthbonda@users.noreply.github.com>
2020-04-29 23:19:31 +0530

    Create cachematrix.R

---
+++ cachematrix.R
@@ -0,0 +1,11 @@
+makeCacheMatrix <- function(mtx = matrix()) {
+    inverse <- NULL
+    set <- function(x) {
+        mtx <<- x;
+        inverse <<- NULL;
+    }
+    get <- function() return(mtx);
+    setinv <- function(inv) inverse <<- inv;
+    getinv <- function() return(inverse);
+    return(list(set = set, get = get, setinv = setinv, getinv = getinv))
+}
